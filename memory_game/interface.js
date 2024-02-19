5b826831a4f1fc5e1d2cb301f90fbb285c0be604
Felipe Silva <92223330+FelipeDaSilvaSantos@users.noreply.github.com>
2024-02-19 19:30:17 -0300

    new array with duplicate elements and different id's

--- memory_game/interface.js
+++ memory_game/interface.js
@@ -1,4 +1,40 @@
-function clique(){
-    let aaa = document.getElementById("8f67d87f89")
-    aaa.classList.toggle("flip")
+let techs = [
+    'bootstrap', 
+    'css', 
+    'electron', 
+    'firebase', 
+    'html', 
+    'javascript', 
+    'jquery', 
+    'mongo', 
+    'node', 
+    'react', 
+]
+
+CreateCardsFromTechs(techs)
+
+function CreateCardsFromTechs(techs){
+    let cards = []
+
+    techs.forEach(card => {
+        cards.push(createDuplicateOfCard(card))
+    })
+
+    console.log(cards)
+}
+
+function createDuplicateOfCard(card){
+    return [{
+        id: createRandomNumber(card),
+        icon: card,
+        flipped: false
+    },{
+        id: createRandomNumber(card),
+        icon: card,
+        flipped: false
+    }]
+}
+
+function createRandomNumber(card){
+    return card +"_" + parseInt(Math.random() * 1000)
 }
