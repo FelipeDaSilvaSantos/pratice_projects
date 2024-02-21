038fa513c66868562499f687d186c18d63d2dc92
Felipe Silva <92223330+FelipeDaSilvaSantos@users.noreply.github.com>
2024-02-21 13:00:51 -0300

    working interface

--- memory_game/interface.js
+++ memory_game/interface.js
@@ -1,4 +1,9 @@
-let techs = [
+const CARD = "card"
+const FRONT = "card_front"
+const BACK = "card_back"
+const ICON = "icon"
+
+let languages = [
     'bootstrap', 
     'css', 
     'electron', 
@@ -11,30 +16,100 @@ let techs = [
     'react', 
 ]
 
-CreateCardsFromTechs(techs)
+let cards = null
 
-function CreateCardsFromTechs(techs){
-    let cards = []
+startGame()
 
-    techs.forEach(card => {
-        cards.push(createDuplicateOfCard(card))
-    })
-
-    console.log(cards)
+function startGame(){
+    let cards = createCardsFromTechs(languages)
+    shuffledCards(cards)
+    createCardsForTheBoard(cards)
 }
 
-function createDuplicateOfCard(card){
+
+//takes the "languages array" and transforms the elements into a new "cards array"
+function createCardsFromTechs(languages){
+    let cards = []
+
+    languages.forEach(lang => {
+        cards.push(createDuplicateOfCard(lang))
+    })
+
+    return cards.flatMap(pair => pair)
+}
+
+function createDuplicateOfCard(lang){
     return [{
-        id: createRandomNumber(card),
-        icon: card,
+        id: createRandomNumber(lang),
+        icon: lang,
         flipped: false
     },{
-        id: createRandomNumber(card),
-        icon: card,
+        id: createRandomNumber(lang),
+        icon: lang,
         flipped: false
     }]
 }
 
-function createRandomNumber(card){
-    return card +"_" + parseInt(Math.random() * 1000)
+function createRandomNumber(lang){
+    return lang + "_" + parseInt(Math.random() * 1000)
+}
+
+
+function shuffledCards(cards){
+    let currentIndex = cards.length //20
+    let randomIndex = 0
+    while (currentIndex != 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex)
+ 
+        currentIndex--
+
+        [cards[currentIndex], cards[randomIndex]] = [cards[randomIndex], cards[currentIndex]]
+    }
+}
+
+// HTML da classe "card"
+function createCardsForTheBoard(cards){
+    let board = document.getElementById("gameBoard")
+
+    cards.forEach(card => {
+
+        let htmlCard = document.createElement("div")
+        htmlCard.id = card.id
+        htmlCard.classList.add(CARD)
+        htmlCard.dataset.icon = card.icon
+        
+        createCardElement(card, htmlCard)
+
+        htmlCard.addEventListener('click', flipCard)
+        board.appendChild(htmlCard)
+
+    })
+}
+
+
+function createCardElement(card, htmlCard){
+    createCardFace(FRONT, card, htmlCard)
+    createCardFace(BACK, card, htmlCard)
+}
+
+function createCardFace(face, card, htmlCard){
+    let cardElements = document.createElement("div")
+    cardElements.classList.add(face)
+    if(face === FRONT){
+        let cardFace = document.createElement("img")
+        cardFace.classList.add(ICON)
+        cardFace.src = "images_game/" + card.icon + ".png"
+        cardElements.appendChild(cardFace)
+        
+    }else if(face === BACK){
+        cardElements.innerHTML = "&lt/&gt"
+    }else{
+        cardElements.iinerHTML = "?"
+    }
+    htmlCard.appendChild(cardElements) //a div pai com class=card recebe como filho a div se é FRONT ou BACK
+
+}
+
+function flipCard(){
+    this.classList.add('flip')
 }
